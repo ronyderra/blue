e516bd00c8d8533f87b6cc2bea645bd4596faa21
Rony Derra <derrarony@gmail.com>
2022-03-20 19:42:23 +0200

    done

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [resualt, setResualt] = useState('waiting for u')
+  const [resualt, setResualt] = useState('We Are Waiting For You')
+  const [intrest, setIntrest] = useState('')
 
   const handleChange = (event) => {
     const percentOfIntrest = 17
     let number = +event.target.value
     let percent = (percentOfIntrest / 100) * number;
     setResualt(percent + number)
+    setIntrest(parseFloat(percent.toFixed(2)))
   }
 
   return (
     <div className="App">
+      <h1>Intrest Calculater</h1>
+      <h3>Intrest is on 17%</h3>
       <br></br>
       <input type='number' onChange={(event) => handleChange(event)} />
       <br></br>
+      <br></br>
       <div>Total Sum: {resualt}</div>
-
+      <br></br>
+      <div>Total Intrest: {intrest}</div>
     </div>
   );
 }
