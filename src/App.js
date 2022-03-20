75ff4303de24f0ce3b614a1e762cbb4dd0f1a892
Rony Derra <derrarony@gmail.com>
2022-03-20 19:13:27 +0200

    calculater

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [resualt, setResualt] = useState('waiting for u')
+
+  const handleChange = (event) => {
+    const percentOfIntrest = 17
+    let number = +event.target.value
+    let percent = (percentOfIntrest / 100) * number;
+    setResualt(percent + number)
+  }
+
   return (
     <div className="App">
+      <br></br>
+      <input type='number' onChange={(event) => handleChange(event)} />
+      <br></br>
+      <div>Total Sum: {resualt}</div>
 
     </div>
   );
 }
-
 export default App;
